5cd985e8a2c7544d8ac1873cbc44445bfdb443f0
Chris Nielsen <chris.nielsen@flite.com>
2016-01-22 12:20:04 -0800

    Initial import of strongswanaws

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'strongswanaws'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures strongswanaws'
+long_description 'Installs/Configures strongswanaws'
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: strongswanaws
+# Recipe:: default
+#
+# Copyright (C) 2016 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
